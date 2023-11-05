2f91cc0ca9dc68a93e8569b629d5c8d7c62fe5f6
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2023-11-05 11:44:05 +0000

    Creation of Activities and bottom navigation bar in the MainActivity

--- app/src/main/java/com/example/dssmv_projectdroid_1220971_1220918/MainActivity.java
+++ app/src/main/java/com/example/dssmv_projectdroid_1220971_1220918/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dssmv_projectdroid_1220971_1220918;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSearchActivity(View v) {
+        Intent i = new Intent(this,SearchActivity.class);
+        startActivity(i);
+    }
+
+    public void launchReviewsActivity(View v) {
+        Intent i = new Intent(this,ReviewsActivity.class);
+        startActivity(i);
+    }
+
+    public void launchCheckedOutActivity(View v) {
+        Intent i = new Intent(this,CheckedOutActivity.class);
+        startActivity(i);
+    }
+
+    public void launchHistoryActivity(View v) {
+        Intent i = new Intent(this,HistoryActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/dssmv_projectdroid_1220971_1220918/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.dssmv_projectdroid_1220971_1220918;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
