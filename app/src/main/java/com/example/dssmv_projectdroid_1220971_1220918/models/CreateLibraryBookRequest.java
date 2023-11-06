974e8b281caf54e5b88fda9da8f615c4518e6c99
João Fonseca <1220918@isep.ipp.pt>
2023-11-06 22:24:45 +0000

    Addition of getters and setters for models

---
+++ app/src/main/java/com/example/dssmv_projectdroid_1220971_1220918/models/CreateLibraryBookRequest.java
@@ -0,0 +1,14 @@
+package com.example.dssmv_projectdroid_1220971_1220918.models;
+
+public class CreateLibraryBookRequest {
+    private int stock;
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
+
--- app/src/main/java/com/example/dssmv_projectdroid_1220971_1220918/models/CreateLibraryRequest.java
+++ app/src/main/java/com/example/dssmv_projectdroid_1220971_1220918/models/CreateLibraryRequest.java
@@ -6,4 +6,48 @@ public class CreateLibraryRequest {
     private String name;
     private String openDays;
     private LocalTime openTime;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public LocalTime getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(LocalTime closeTime) {
+        this.closeTime = closeTime;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getOpenDays() {
+        return openDays;
+    }
+
+    public void setOpenDays(String openDays) {
+        this.openDays = openDays;
+    }
+
+
+    public LocalTime getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(LocalTime openTime) {
+        this.openTime = openTime;
+    }
 }
